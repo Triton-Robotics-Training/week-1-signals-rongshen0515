8e96e9467fd2ab7b0fdd390766b6cc4950592010
Rong Shen Ng <71053647+rongshen0515@users.noreply.github.com>
2025-10-19 11:09:31 -0700

    week1_signals_rongshen0515_TR

--- week1_exercise1.cpp
+++ week1_exercise1.cpp
@@ -4,10 +4,14 @@
 //IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
+DigitalOut led(LED1);
+
 int main() {
     while (1) {
         printf("Week 1 Exercise 1");
 
+        led = !led;
+        printf("Blink! LED is now %d\n", led.read());
         // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
         wait_ms(500); 
     }
