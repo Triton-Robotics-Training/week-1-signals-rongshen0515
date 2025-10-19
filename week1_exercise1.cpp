a1567d8728bc84023fba62c543f8a3db6950de05
Rong Shen Ng <71053647+rongshen0515@users.noreply.github.com>
2025-10-19 11:42:48 -0700

    Update week1_exercise1.cpp

--- week1_exercise1.cpp
+++ week1_exercise1.cpp
@@ -4,14 +4,24 @@
 //IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
-DigitalOut led(LED1);
+DigitalIn input(BUTTON1);
+DigitalOut output(LED1);
+int x = input.read(); 
+int y = output.read();
 
 int main() {
     while (1) {
         printf("Week 1 Exercise 1");
-
-        led = !led;
-        printf("Blink! LED is now %d\n", led.read());
+        printf("Button = %d, LED = %d\n", input.read(), output.read());
+        if (input == 1) {
+            output = 1;
+        }
+        else if (input == 0 && output == 1) {
+            output = 0;
+        }
+        else {
+            output = 0;
+        }
         // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
         wait_ms(500); 
     }
