0e3db33c1c7988900c293d926e3c6463879c817a
Rong Shen Ng <71053647+rongshen0515@users.noreply.github.com>
2025-10-19 16:17:06 -0700

    Update week1_exercise2.cpp

--- week1_exercise2.cpp
+++ week1_exercise2.cpp
@@ -4,11 +4,23 @@
 //IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
+AnalogIn pot(p15);      
+DigitalOut led(LED1);   
+
 int main() {
     while (1) {
-        printf("Week 1 Exercise 2");
+        printf("Week 1 Exercise 2\n");
+        
+        double x = pot.read();
+        double on_time = x * 2000.0f;          
+        double off_time = (1.0f - x) * 2000.0f; 
 
-        // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
-        wait_ms(500); 
+        led = 1;
+        printf("The potentiometer reads = %f | LED turned on for %f ms\n", x, on_time);
+        wait_ms(on_time);
+
+        led = 0;
+        printf("The potentiometer reads = %f | LED turned off for %f ms\n\n", x, off_time);
+        wait_ms(off_time);
     }
 }
